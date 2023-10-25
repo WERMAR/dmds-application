11feb17f30baea468ac7a03b9189cb964185bbfc
wermar <mail@marcelwernisch.de>
2023-10-25 21:46:11 +0200

    Add UE2 Application

--- src/main/java/de/wermar/dmds/Main.java
+++ src/main/java/de/wermar/dmds/Main.java
@@ -2,23 +2,106 @@ package de.wermar.dmds;
 
 import de.wermar.dmds.jdbc.JDBCConnector;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    private final JDBCConnector jdbcConnector = new JDBCConnector();
+    private static boolean cancel = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         var instance = new Main();
         var scanner = new Scanner(System.in);
         System.out.println("Wähle Szenario aus: ");
-        System.out.println("""
+        System.out.print("""
                 #1 - JDBCConnection Establishment
+                #2 - Get available Tables in AWC Schema
+                #3 - SELECT, UPDATE, INSERT Data into the AWC Schema
                 """);
+        System.out.print("Szenario: ");
         var input = scanner.nextInt();
+        scanner.nextLine();
         switch (input) {
-            case 1 -> instance.jdbcConnector.establishConnection();
-
+            case 1 -> JDBCConnector.instance().establishConnection();
+            case 2 -> JDBCConnector.instance().loadTablesFromAWC();
+            case 3 -> startDBModifier(scanner);
         }
+        JDBCConnector.instance().closeConnection();
+        scanner.close();
+    }
+
+    private static void startDBModifier(Scanner scanner) {
+        System.out.print("""
+                Which Operation you will want to do: 
+                #1 - SELECT some Data include a WHERE Clause
+                #2 - INSERT Data to a specific Table
+                #3 - UPDATE Data to a specific Table with a WHERE Clause
+                #4 - Abort Sequence with C
+                Please Choose S(ELECT), U(PDATE), I(NSERT), C(ANCEL)
+                """);
+        while (!cancel) {
+            System.out.print("Next Execution: ");
+            var input = scanner.nextLine();
+            if (input.length() < 0)
+                throw new RuntimeException("Further processing is not possible");
+            var selection = input.toUpperCase().charAt(0);
+            switch (selection) {
+                case 'S' -> doSelectStatement(scanner);
+                case 'U' -> doUpdateStatement(scanner);
+                case 'I' -> doInsertStatement(scanner);
+                case 'C' -> doCancel();
+            }
+        }
+    }
+
+    private static void doInsertStatement(Scanner scanner) {
+        System.out.print("Table-Name to Insert the Data: ");
+        var tableName = scanner.nextLine();
+        List<String> values = new ArrayList<>();
+        boolean breakLoop = false;
+        while (!breakLoop) {
+            System.out.print("Add the Inserted Values - Please without () | Or Break with CANCEL: ");
+            var input = scanner.nextLine();
+            if ("CANCEL".equalsIgnoreCase(input)) {
+                breakLoop = true;
+                break;
+            }
+            var finalInput = "(" + input + ")";
+            values.add(finalInput);
+        }
+        JDBCConnector.instance().doInsert(tableName, values);
+    }
+
+    private static void doUpdateStatement(Scanner scanner) {
+        System.out.print("Table-Name for the Selection: ");
+        var tableName = scanner.nextLine();
+        System.out.print("WHERE Conditions: ");
+        var whereCondition = scanner.nextLine();
+        if (whereCondition.length() == 0)
+            throw new RuntimeException("No WHERE Condition is not allowed");
+        System.out.print("Field which should be Updated: ");
+        var field = scanner.nextLine();
+        System.out.print("Value of the Update: ");
+        var valueOfUpdate = scanner.nextLine();
+
+        JDBCConnector.instance().doUpdate(tableName, whereCondition, field, valueOfUpdate);
+    }
+
+    private static void doSelectStatement(Scanner scanner) {
+        System.out.print("Table-Name for the Selection: ");
+        var tableName = scanner.nextLine();
+        System.out.print("Field-Names for the Selection divided by ',' or using '*' for All: ");
+        var fieldNames = scanner.nextLine();
+        System.out.print("WHERE Conditions: ");
+        var whereCondition = scanner.nextLine();
+
+        JDBCConnector.instance().doSelect(tableName, fieldNames, whereCondition);
+    }
+
+    private static void doCancel() {
+        cancel = true;
     }
 }
--- src/main/java/de/wermar/dmds/jdbc/JDBCConnector.java
+++ src/main/java/de/wermar/dmds/jdbc/JDBCConnector.java
@@ -3,11 +3,144 @@ package de.wermar.dmds.jdbc;
 import de.wermar.dmds.jdbc.credentials.Credentials;
 import de.wermar.dmds.jdbc.credentials.CredentialsReader;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JDBCConnector {
+
+    private static JDBCConnector jdbcConnectorInstance;
+
+    private Connection currConnection;
+
+    public static final JDBCConnector instance() {
+        if (jdbcConnectorInstance == null)
+            jdbcConnectorInstance = new JDBCConnector();
+        return jdbcConnectorInstance;
+    }
+
     private final CredentialsReader credentialsReader = new CredentialsReader();
+
+    private JDBCConnector() {
+        // private Constructor due Singleton-Pattern Reasons
+    }
 
     public void establishConnection() {
         Credentials credentials = this.credentialsReader.readCredentials();
-        System.out.println("Credentials: " + credentials.getDBUrl());
+        try {
+            var connection = DriverManager.getConnection(credentials.getDBUrl(), credentials.getUser(), credentials.getPassword());
+            if (connection != null) {
+                this.currConnection = connection;
+                System.out.println("Connection established");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadTablesFromAWC() {
+        this.establishConnection();
+        System.out.println("Start Executing Query to fetch available Tables");
+        try {
+            var statement = this.currConnection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM information_schema.TABLES where TABLE_SCHEMA like 'awc'");
+            extractResult(result);
+            result.close();
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void extractResult(ResultSet result) throws SQLException {
+        System.out.println("Result: ");
+        while (result.next()) {
+            System.out.println("Table: {" + result.getString("TABLE_NAME") + "} exists");
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        this.currConnection.close();
+    }
+
+    public void doInsert(String tableName, List<String> values) {
+        this.establishConnection();
+        try {
+            var statement = this.currConnection.createStatement();
+            StringBuilder query = new StringBuilder("INSERT INTO ");
+            query.append(tableName);
+            query.append(" VALUES ");
+
+            values.stream().forEach(valueObject -> {
+                query.append(valueObject);
+                query.append(",");
+            });
+            query.replace(query.lastIndexOf(","), query.lastIndexOf(",") + 1, ";");
+            System.out.println("Executed Query: { " + query.toString() + " }");
+            statement.executeUpdate(query.toString());
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
+        }
+    }
+
+
+    public void doSelect(String tableName, String fieldNames, String whereCondition) {
+        this.establishConnection();
+        try {
+            var statement = this.currConnection.createStatement();
+            var query = "SELECT ";
+            query += fieldNames;
+            query += " FROM ";
+            query += tableName;
+            if (whereCondition.length() > 0) {
+                query += " WHERE ";
+                query += whereCondition;
+            }
+            var result = statement.executeQuery(query);
+            extractResult(result, fieldNames);
+            statement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void extractResult(ResultSet result, String fieldNames) throws SQLException {
+        List<String> fieldNamesSplitted = new ArrayList<>();
+        if (!"*".equals(fieldNames)) {
+            fieldNamesSplitted = Arrays.stream(fieldNames.split(",")).map(e -> e.trim()).toList();
+        } else {
+            int columnCount = result.getMetaData().getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                fieldNamesSplitted.add(result.getMetaData().getColumnName(i));
+            }
+        }
+        int i = 1;
+        while (result.next()) {
+            StringBuilder builder = new StringBuilder("{ ");
+            fieldNamesSplitted.stream().forEach(columnName -> {
+                try {
+                    builder.append(columnName + " : ");
+                    builder.append(result.getObject(columnName) + " | ");
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            builder.append(" }");
+            System.out.println("Result [" + i + " ]: " + builder.toString());
+            i++;
+        }
+    }
+
+    public void doUpdate(String tableName, String whereCondition, String field, String valueOfUpdate) {
+        this.establishConnection();
+        var query = "UPDATE " + tableName + " SET " + field + " = ? WHERE " + whereCondition;
+        try (var preparedStatement = this.currConnection.prepareStatement(query)) {
+            System.out.println("Execute Query: { " + query + " }");
+            preparedStatement.setString(1, valueOfUpdate);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
